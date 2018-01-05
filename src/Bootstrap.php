1e6134f1f9f924c6f2a9d5caa5d82c522cebccae
James Miranda <jameswpm@gmail.com>
2018-01-05 21:37:14 -0200

    Include composer and error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,29 @@
 <?php declare(strict_types = 1);
 
-echo 'Hello World!';
+namespace RandomFlix;
+
+use Exception;
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
+
+
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw Exception;
